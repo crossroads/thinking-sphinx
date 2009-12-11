75162366a7cebc06e5522707a6400aba7db81a90
warp <warp@warp-laptop.crossroadsint.org>
2009-12-11 14:33:16 +0800

    Changed gem name for distribution

--- tasks/distribution.rb
+++ tasks/distribution.rb
@@ -5,13 +5,13 @@ desc 'Generate documentation'
 YARD::Rake::YardocTask.new
 
 Jeweler::Tasks.new do |gem|
-  gem.name        = "thinking-sphinx"
+  gem.name        = "warp-thinking-sphinx"
   gem.summary     = "ActiveRecord/Rails Sphinx library"
   gem.description = "A concise and easy-to-use Ruby library that connects ActiveRecord to the Sphinx search daemon, managing configuration, indexing and searching."
   gem.author      = "Pat Allan"
   gem.email       = "pat@freelancing-gods.com"
   gem.homepage    = "http://ts.freelancing-gods.com"
-    
+
   # s.rubyforge_project = "thinking-sphinx"
   gem.files     = FileList[
     "rails/*.rb",
@@ -26,14 +26,16 @@ Jeweler::Tasks.new do |gem|
     "features/**/*",
     "spec/**/*_spec.rb"
   ]
-  
+
   gem.add_dependency 'activerecord', '>= 1.15.6'
   gem.add_dependency 'riddle',       '>= 1.0.8'
   gem.add_dependency 'after_commit', '>= 1.0.5'
-  
+
   gem.post_install_message = <<-MESSAGE
 If you're upgrading, you should read this:
 http://freelancing-god.github.com/ts/en/upgrading.html
 
 MESSAGE
 end
+
+Jeweler::GemcutterTasks.new
